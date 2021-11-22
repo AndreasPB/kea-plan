4f17ddc9a1dce98ecc9a242fb22e56a0cd9ea108
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2021-11-22 15:43:45 +0100

    Run prettier post-rebase

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,10 +1,10 @@
 <script>
-	import Navbar from '../components/navbar.svelte';
+  import Navbar from "../components/navbar.svelte"
 </script>
 
 <div class="flex flex-col h-screen justify-between">
-	<Navbar />
-	<div class="mb-auto container mx-auto">
-		<slot />
-	</div>
+  <Navbar />
+  <div class="mb-auto container mx-auto">
+    <slot />
+  </div>
 </div>
