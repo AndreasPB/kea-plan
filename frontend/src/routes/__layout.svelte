ad6c0b38a81cef24416dc8f814d8e61be994e457
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2021-11-22 11:03:45 +0100

    Add navbar + layout

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Navbar from '../components/navbar.svelte';
+</script>
+
+<div class="flex flex-col h-screen justify-between">
+	<Navbar />
+	<div class="mb-auto container mx-auto">
+		<slot />
+	</div>
+</div>
