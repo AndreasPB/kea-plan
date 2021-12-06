4a5a9165b3e030078a40307c42807becc98bbbed
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2021-12-06 21:00:17 +0100

    Add tests for new GET semester endpoints

--- backend/tests/statistics/test_semester.py
+++ backend/tests/statistics/test_semester.py
@@ -13,6 +13,26 @@ def setup_db():
     client.delete("/statistics/test/semesters")
 
 
+def test_get_semesters():
+    response = client.get("/statistics/semesters")
+    assert response.status_code == 200
+
+    data = response.json()
+    for semester in data:
+        assert Semester(**semester)
+
+
+def test_get_semester():
+    response = client.get("/statistics/semester/1")
+    assert response.status_code == 200
+
+    data = response.json()
+    assert Semester(**data)
+
+    not_found_reponse = client.get("/statistics/semester/9001")
+    assert not_found_reponse.status_code == 404
+
+
 def test_create_test_semesters():
     response = client.post("/statistics/test/semesters")
     assert response.status_code == 200
