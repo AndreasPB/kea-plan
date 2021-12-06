f233adce1d5b2f2f15ae774e2d306ff59f12351b
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2021-12-06 17:30:59 +0100

    Add student and teacher statistics mock views

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,9 +15,15 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:2000",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
