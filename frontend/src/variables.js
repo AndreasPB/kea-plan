6117706edd987f1639b36a1082e356ec04e8c86c
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-01-04 17:52:47 +0100

    Swap hardcoded variables with .env

---
+++ frontend/src/variables.js
@@ -0,0 +1,7 @@
+// TODO: Make this work somehow - Not accepted by jest ATM
+// const API_PATH = import.meta.env.VITE_API_PATH || "http://localhost:2000"
+const API_PATH = "http://localhost:2000"
+
+export const variables = {
+  apiPath: API_PATH,
+}
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,7 @@
 import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
@@ -11,5 +11,3 @@ const config = {
     target: "#svelte",
   },
 }
-
-export default config
